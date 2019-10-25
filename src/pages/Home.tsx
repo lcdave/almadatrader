7a677a46e3a7442959177f326638526851e03b97
Davide Lo Cascio <locascio.davide@gmail.com>
2019-10-25 13:11:21 +0200

    implements list view and button components (wip)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonModal, IonButton, IonList, IonItem, IonLabel, IonContent, IonIcon, IonFab, IonFabButton} from '@ionic/react';
+import React, { useState } from 'react';
+import { add } from 'ionicons/icons';
+
 
 const Home: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>Einkaufsliste</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
+      <IonContent>
+    {/*-- List of Text Items --*/}
+    <IonList>
+      <IonItem>
+        <IonLabel>Vidal</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Sancho</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Coman</IonLabel>
+      </IonItem>
+      <IonModal isOpen={showModal}>
+        <p>This is modal content</p>
+        <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
+      </IonModal>
+      <IonButton onClick={() => setShowModal(true)}>
+        <IonIcon icon={add} />
+      </IonButton>
+      <IonFab vertical="bottom" horizontal="end" slot="fixed">
+      <IonFabButton>
+        <IonIcon icon={add} />
+      </IonFabButton>
+    </IonFab>
+      </IonList>
+  </IonContent>
     </IonPage>
   );
 };
