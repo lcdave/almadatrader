dfba96bd6dd282280d01834850bcf37e7bda7839
Davide Lo Cascio <locascio.davide@gmail.com>
2019-10-30 13:09:58 +0100

    implements default list view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { IonModal, IonButton, IonList, IonItem, IonLabel, IonContent, IonIcon, IonFab, IonFabButton} from '@ionic/react';
+import { IonModal, IonButton, IonList, IonItem, IonLabel, IonContent, IonIcon, IonFab, IonFabButton,
+IonGrid, IonRow, IonCol} from '@ionic/react';
 import React, { useState } from 'react';
 import { add } from 'ionicons/icons';
 
@@ -16,27 +17,85 @@ const Home: React.FC = () => {
       <IonContent>
     {/*-- List of Text Items --*/}
     <IonList>
-      <IonItem>
-        <IonLabel>Vidal</IonLabel>
+      <IonItem class="list_header">
+        <IonGrid>
+          <IonRow>
+            <IonCol>Spieler</IonCol>
+            <IonCol>Anzahl</IonCol>
+            <IonCol>Preis</IonCol>
+          </IonRow>
+        </IonGrid>
       </IonItem>
       <IonItem>
-        <IonLabel>Sancho</IonLabel>
+      <IonGrid>
+          <IonRow>
+            <IonCol>Sancho</IonCol>
+            <IonCol>1x</IonCol>
+            <IonCol>4'900</IonCol>
+          </IonRow>
+        </IonGrid>
       </IonItem>
       <IonItem>
-        <IonLabel>Coman</IonLabel>
+      <IonGrid>
+          <IonRow>
+            <IonCol>Coman</IonCol>
+            <IonCol>5x</IonCol>
+            <IonCol>6'900</IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonItem>
+      <IonItem>
+      <IonGrid>
+          <IonRow>
+            <IonCol>Havertz</IonCol>
+            <IonCol>5x</IonCol>
+            <IonCol>5'900</IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonItem>
+      <IonItem>
+      <IonGrid>
+          <IonRow>
+            <IonCol>Rodri</IonCol>
+            <IonCol>10x</IonCol>
+            <IonCol>6'900</IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonItem>
+      <IonItem>
+      <IonGrid>
+          <IonRow>
+            <IonCol>Felipe Anderson</IonCol>
+            <IonCol>5x</IonCol>
+            <IonCol>14'900</IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonItem>
+      <IonItem>
+      <IonGrid>
+          <IonRow>
+            <IonCol>Gnabry</IonCol>
+            <IonCol>10x</IonCol>
+            <IonCol>8'900</IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonItem>
+      <IonItem>
+      <IonGrid>
+          <IonRow>
+            <IonCol>Lucas Moura</IonCol>
+            <IonCol>10x</IonCol>
+            <IonCol>30'000</IonCol>
+          </IonRow>
+        </IonGrid>
       </IonItem>
       <IonModal isOpen={showModal}>
         <p>This is modal content</p>
         <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
       </IonModal>
-      <IonButton onClick={() => setShowModal(true)}>
-        <IonIcon icon={add} />
+      <IonButton expand="block" mode="md">
+        <IonIcon icon={add} onClick={() => setShowModal(true)} />
       </IonButton>
-      <IonFab vertical="bottom" horizontal="end" slot="fixed">
-      <IonFabButton>
-        <IonIcon icon={add} />
-      </IonFabButton>
-    </IonFab>
       </IonList>
   </IonContent>
     </IonPage>
